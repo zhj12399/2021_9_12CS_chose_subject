21ac8b0dd570bd474f4f3a4de294356266a60699
zhj <zhj727534681@163.com>
2021-09-12 22:38:00 +0800

    First

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace chosesubject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_studentid = new System.Windows.Forms.TextBox();
+            this.label_studentid = new System.Windows.Forms.Label();
+            this.button_SelectName = new System.Windows.Forms.Button();
+            this.label_studentname_show = new System.Windows.Forms.Label();
+            this.label_studentname = new System.Windows.Forms.Label();
+            this.DataGridView_top = new System.Windows.Forms.DataGridView();
+            this.DataGridView_down = new System.Windows.Forms.DataGridView();
+            this.label_top = new System.Windows.Forms.Label();
+            this.label_down = new System.Windows.Forms.Label();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button_select = new System.Windows.Forms.Button();
+            this.label_mid = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_down)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_studentid
+            // 
+            this.textBox_studentid.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_studentid.Location = new System.Drawing.Point(129, 50);
+            this.textBox_studentid.Name = "textBox_studentid";
+            this.textBox_studentid.Size = new System.Drawing.Size(196, 29);
+            this.textBox_studentid.TabIndex = 6;
+            // 
+            // label_studentid
+            // 
+            this.label_studentid.AutoSize = true;
+            this.label_studentid.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_studentid.Location = new System.Drawing.Point(69, 53);
+            this.label_studentid.Name = "label_studentid";
+            this.label_studentid.Size = new System.Drawing.Size(69, 19);
+            this.label_studentid.TabIndex = 5;
+            this.label_studentid.Text = "学号：";
+            // 
+            // button_SelectName
+            // 
+            this.button_SelectName.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_SelectName.Location = new System.Drawing.Point(350, 47);
+            this.button_SelectName.Name = "button_SelectName";
+            this.button_SelectName.Size = new System.Drawing.Size(99, 40);
+            this.button_SelectName.TabIndex = 4;
+            this.button_SelectName.Text = "查询选课";
+            this.button_SelectName.UseVisualStyleBackColor = true;
+            this.button_SelectName.Click += new System.EventHandler(this.button_SelectName_Click);
+            // 
+            // label_studentname_show
+            // 
+            this.label_studentname_show.AutoSize = true;
+            this.label_studentname_show.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_studentname_show.Location = new System.Drawing.Point(125, 115);
+            this.label_studentname_show.Name = "label_studentname_show";
+            this.label_studentname_show.Size = new System.Drawing.Size(229, 19);
+            this.label_studentname_show.TabIndex = 8;
+            this.label_studentname_show.Text = "输入学号并点击查询选课";
+            // 
+            // label_studentname
+            // 
+            this.label_studentname.AutoSize = true;
+            this.label_studentname.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_studentname.Location = new System.Drawing.Point(69, 115);
+            this.label_studentname.Name = "label_studentname";
+            this.label_studentname.Size = new System.Drawing.Size(69, 19);
+            this.label_studentname.TabIndex = 7;
+            this.label_studentname.Text = "姓名：";
+            // 
+            // DataGridView_top
+            // 
+            this.DataGridView_top.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView_top.Location = new System.Drawing.Point(47, 149);
+            this.DataGridView_top.Name = "DataGridView_top";
+            this.DataGridView_top.RowTemplate.Height = 23;
+            this.DataGridView_top.Size = new System.Drawing.Size(656, 228);
+            this.DataGridView_top.TabIndex = 9;
+            // 
+            // DataGridView_down
+            // 
+            this.DataGridView_down.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView_down.Location = new System.Drawing.Point(47, 432);
+            this.DataGridView_down.Name = "DataGridView_down";
+            this.DataGridView_down.RowTemplate.Height = 23;
+            this.DataGridView_down.Size = new System.Drawing.Size(656, 328);
+            this.DataGridView_down.TabIndex = 10;
+            // 
+            // label_top
+            // 
+            this.label_top.AutoSize = true;
+            this.label_top.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_top.Location = new System.Drawing.Point(12, 205);
+            this.label_top.Name = "label_top";
+            this.label_top.Size = new System.Drawing.Size(29, 76);
+            this.label_top.TabIndex = 11;
+            this.label_top.Text = "已\r\n选\r\n课\r\n程\r\n";
+            // 
+            // label_down
+            // 
+            this.label_down.AutoSize = true;
+            this.label_down.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_down.Location = new System.Drawing.Point(12, 556);
+            this.label_down.Name = "label_down";
+            this.label_down.Size = new System.Drawing.Size(29, 76);
+            this.label_down.TabIndex = 12;
+            this.label_down.Text = "待\r\n选\r\n课\r\n程\r\n";
+            // 
+            // button_delete
+            // 
+            this.button_delete.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_delete.Location = new System.Drawing.Point(159, 790);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(99, 40);
+            this.button_delete.TabIndex = 13;
+            this.button_delete.Text = "退课";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // button_select
+            // 
+            this.button_select.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_select.Location = new System.Drawing.Point(424, 790);
+            this.button_select.Name = "button_select";
+            this.button_select.Size = new System.Drawing.Size(99, 40);
+            this.button_select.TabIndex = 14;
+            this.button_select.Text = "选课";
+            this.button_select.UseVisualStyleBackColor = true;
+            this.button_select.Click += new System.EventHandler(this.button_select_Click);
+            // 
+            // label_mid
+            // 
+            this.label_mid.AutoSize = true;
+            this.label_mid.Font = new System.Drawing.Font("楷体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_mid.Location = new System.Drawing.Point(204, 394);
+            this.label_mid.Name = "label_mid";
+            this.label_mid.Size = new System.Drawing.Size(299, 19);
+            this.label_mid.TabIndex = 15;
+            this.label_mid.Text = "已选课程数量：0 ，已选学分：0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 851);
+            this.Controls.Add(this.label_mid);
+            this.Controls.Add(this.button_select);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.label_down);
+            this.Controls.Add(this.label_top);
+            this.Controls.Add(this.DataGridView_down);
+            this.Controls.Add(this.DataGridView_top);
+            this.Controls.Add(this.label_studentname_show);
+            this.Controls.Add(this.label_studentname);
+            this.Controls.Add(this.textBox_studentid);
+            this.Controls.Add(this.label_studentid);
+            this.Controls.Add(this.button_SelectName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView_down)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_studentid;
+        private System.Windows.Forms.Label label_studentid;
+        private System.Windows.Forms.Button button_SelectName;
+        private System.Windows.Forms.Label label_studentname_show;
+        private System.Windows.Forms.Label label_studentname;
+        private System.Windows.Forms.DataGridView DataGridView_top;
+        private System.Windows.Forms.DataGridView DataGridView_down;
+        private System.Windows.Forms.Label label_top;
+        private System.Windows.Forms.Label label_down;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button_select;
+        private System.Windows.Forms.Label label_mid;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chosesubject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitDataGridView();
+        }
+
+        string studentid;
+        string sqlconnectioncommand = "Data Source=182.92.76.25;Initial Catalog=timetable;Uid=SA;Pwd=ZHJzhj2121";
+
+        int ischosenum = 0;
+        int ischosecredit = 0;
+        private void button_SelectName_Click(object sender, EventArgs e)
+        {
+            if (textBox_studentid.Text.Length != 0)
+            {
+                studentid = textBox_studentid.Text;
+                string queryString = "SELECT student_name FROM dbo.student where student_id = " + studentid;
+                using (SqlConnection connection = new SqlConnection(sqlconnectioncommand))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            label_studentname_show.Text = (string)reader[0];
+                            updatedatagrip();
+                        }
+                        else
+                        {
+                            label_studentname_show.Text = "查无此人";
+                            MessageBox.Show("查无此人");
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("学号不能为空");
+            }
+        }
+
+        private void updatedatagrip()
+        {
+            ischosenum = 0;
+            ischosecredit = 0;
+
+            //查询该用户已经选的课
+            List<Int32> subject_has_chose = new List<Int32>();
+            string queryString = "SELECT C_ID FROM dbo.subject_chose_" + studentid;
+            using (SqlConnection connection = new SqlConnection(sqlconnectioncommand))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        subject_has_chose.Add((Int32)reader[0]);
+                    }
+                }
+            }
+
+            DataGridView_down.Rows.Clear();
+            DataGridView_top.Rows.Clear();
+
+            //所有课表
+            queryString = "SELECT * FROM dbo.curriculum";
+            using (SqlConnection connection = new SqlConnection(sqlconnectioncommand))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int subjectid = (Int32)reader[0];
+                        Object[] row = new object[6];
+                        row[0] = subjectid;
+                        row[1] = (string)reader[1];
+                        row[2] = (string)reader[2];
+                        int credit = (Int32)reader[3];
+                        row[3] = credit;
+                        row[4] = (string)reader[4];
+                        if (subject_has_chose.Exists(x => x == subjectid))
+                        {//已经选的课
+                            DataGridView_top.Rows.Add(row);
+                            ischosenum++;
+                            ischosecredit += credit;
+                        }
+                        else
+                        {
+                            DataGridView_down.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+            label_mid.Text = "已选课程数量：" + ischosenum + " ，已选学分：" + ischosecredit;
+        }
+
+        private void InitDataGridView()
+        {
+            //上，已选课程
+            DataGridView_top.ColumnCount = 5;
+            DataGridView_top.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
+            DataGridView_top.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            DataGridView_top.ColumnHeadersDefaultCellStyle.Font = new Font(DataGridView_top.Font, FontStyle.Bold);
+            DataGridView_top.Columns[0].Name = "课程ID";
+            DataGridView_top.Columns[1].Name = "课程名称";
+            DataGridView_top.Columns[2].Name = "授课老师";
+            DataGridView_top.Columns[3].Name = "学分";
+            DataGridView_top.Columns[4].Name = "时间";
+            DataGridView_top.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            DataGridView_top.MultiSelect = false;
+            //下，待选课程
+            DataGridView_down.ColumnCount = 5;
+            DataGridView_down.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
+            DataGridView_down.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            DataGridView_down.ColumnHeadersDefaultCellStyle.Font = new Font(DataGridView_top.Font, FontStyle.Bold);
+            DataGridView_down.Columns[0].Name = "课程ID";
+            DataGridView_down.Columns[1].Name = "课程名称";
+            DataGridView_down.Columns[2].Name = "授课老师";
+            DataGridView_down.Columns[3].Name = "学分";
+            DataGridView_down.Columns[4].Name = "时间";
+            DataGridView_down.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            DataGridView_down.MultiSelect = true;
+
+            DataGridView_top.Columns[0].DisplayIndex = 0;
+            DataGridView_top.Columns[1].DisplayIndex = 1;
+            DataGridView_top.Columns[2].DisplayIndex = 2;
+            DataGridView_top.Columns[3].DisplayIndex = 3;
+            DataGridView_top.Columns[4].DisplayIndex = 4;
+
+            DataGridView_down.Columns[0].DisplayIndex = 0;
+            DataGridView_down.Columns[1].DisplayIndex = 1;
+            DataGridView_down.Columns[2].DisplayIndex = 2;
+            DataGridView_down.Columns[3].DisplayIndex = 3;
+            DataGridView_down.Columns[4].DisplayIndex = 4;
+        }
+
+        private void button_select_Click(object sender, EventArgs e)
+        {
+            if (DataGridView_down.SelectedRows.Count + ischosenum >= 3 && DataGridView_down.SelectedRows.Count + ischosenum <= 5)
+            {
+                int chosecredit = 0;
+                Console.WriteLine(DataGridView_down.SelectedRows);
+                foreach (DataGridViewRow selectrow in DataGridView_down.SelectedRows)
+                {
+                    chosecredit += (int)selectrow.Cells[3].Value;
+                }
+
+                if (chosecredit +ischosecredit >= 8 && chosecredit + ischosecredit <= 12)
+                {
+                    using (SqlConnection connection = new SqlConnection(sqlconnectioncommand))
+                    {
+                        connection.Open();
+                        foreach (DataGridViewRow selectrow in DataGridView_down.SelectedRows)
+                        {
+                            if (selectrow.Cells[0].Value != null)
+                            {
+                                int choseid = (int)selectrow.Cells[0].Value;
+                                string queryString = "INSERT dbo.subject_chose_" + studentid + " values(" + choseid + ")";
+                                SqlCommand command = new SqlCommand(queryString, connection);
+                                command.ExecuteNonQuery();
+                            }
+                            else
+                            {
+                                MessageBox.Show("请不要选择空行");
+                            }
+                        }
+                    }
+                    updatedatagrip();
+                }
+                else
+                {
+                    MessageBox.Show("您选择了"+ chosecredit + "个学分，请按规定一共选择8-12学分");
+                }
+            }
+            else
+            {
+                MessageBox.Show("您选择了" + DataGridView_down.SelectedRows.Count + "门课，按规定一共选择3-5门课");
+            }
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            if (DataGridView_top.SelectedRows.Count == 1)
+            {
+                using (SqlConnection connection = new SqlConnection(sqlconnectioncommand))
+                {
+                    connection.Open();
+                    foreach (DataGridViewRow selectrow in DataGridView_top.SelectedRows)
+                    {
+                        if(selectrow.Cells[0].Value != null)
+                        {
+                            int choseid = (int)selectrow.Cells[0].Value;
+                            string queryString = "DELETE dbo.subject_chose_" + studentid + " where C_ID = " + choseid;
+                            SqlCommand command = new SqlCommand(queryString, connection);
+                            command.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            MessageBox.Show("请不要选择空值");
+                        }
+                    }
+                }
+                updatedatagrip();
+            }
+            else
+            {
+                MessageBox.Show("您未选择课程");
+            }
+        }
+    }
+}
